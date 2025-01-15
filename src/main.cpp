aec8d9105c744a7866a273cac003542c28c70fa2
yaacoo <or.yaacov@mail.huji.ac.il>
2025-01-15 14:42:30 -0500

    changed delay

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,11 @@
 #define I2C_SCL 22
 
 // Display settings
-#define LED_BUILTIN 2
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
 #define MAX_DEVICES 4
-#define DIN_PIN 23  // MOSI pin for SPI
-#define CLK_PIN 18  // SCK pin for SPI
-#define CS_PIN  5   // SS pin for SPI
+#define DIN_PIN 23  // MOSI 
+#define CLK_PIN 18  // SCK 
+#define CS_PIN  5   // SS 
 
 // DHT settings
 #define DHTPIN 2
@@ -28,7 +27,6 @@ DHT dht(DHTPIN, DHTTYPE);
 MD_Parola display = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 
 // put function declarations here:
-int myFunction(int, int);
 void displayValue(float value, const char* unit);
 float celsiusToFahrenheit(float celsius);
 
@@ -38,10 +36,9 @@ void setup() {
   
   // Initialize LED matrix
   mx.begin();
-  mx.control(MD_MAX72XX::INTENSITY, 2);  // brightness (0-15)
+  mx.control(MD_MAX72XX::INTENSITY, 5);  // brightness 0-15
   mx.clear();
   
-  pinMode(LED_BUILTIN, OUTPUT);
 
   if (!sensor.begin()) {
     Serial.println("Si7021 sensor not found!");
@@ -56,9 +53,7 @@ void setup() {
   display.setSpeed(100);  // Lower = faster
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  
+void loop() {  
   // read temperature and humidity
   float temperature = sensor.readTemperature();
   float humidity = sensor.readHumidity();
@@ -73,36 +68,23 @@ void loop() {
   Serial.println("%");
 
   if (!isnan(humidity) && !isnan(temperature)) {
-    // Alternate between temp and humidity every 2 seconds
+    // Alternate between temp and humidity 
     displayValue(temperature, " C");
-    delay(2000);
+    delay(1500);
     displayValue(fahrenheit, " F");
-    delay(2000);
+    delay(1500);
     displayValue(humidity, " %");
     delay(3000);
   }
-
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
-  // wait for a second
-  delay(1000);
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
-  // wait for a second
-  delay(1000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
 
 void displayValue(float value, const char* unit) {
   char str[10];
   dtostrf(value, 4, 1, str);
   strcat(str, unit);
-  display.setTextAlignment(PA_CENTER);  // Center the text
-  display.print(str);  // Static display
+  display.setTextAlignment(PA_CENTER);  // Center text
+  display.print(str);  // Static 
 }
 
 float celsiusToFahrenheit(float celsius) {
